49c501142c8d6b8906d032d9158c8de9668e5e22
Zeki Mirza <zeki.mirza@googlemail.com>
2021-10-11 00:11:22 +0100

    Made the todo factory function

---
+++ src/components/todo.js
@@ -0,0 +1,24 @@
+const todo = (name,dueDate,...tags) => {
+    const isCompleted = false
+    const tagList = [...tags]
+
+    const setCompleted = () => {
+        isCompleted = true
+    }
+
+    const setUncompleted = () => {
+        isCompleted = false
+    }
+
+    const addTags = (...newTags) => {
+        tagList.push(...newTags)
+    }
+
+    const removeTags = (...remvovedTags) => {
+        for(let i = 0; i < removedTags.length; i++){
+            tagList.splice(i,1)
+        }
+    }
+
+    return {name,dueDate,isCompleted,tagList,addTags,removeTags}
+}
