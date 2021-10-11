3db960fccc8fe2558cf0695848100f91f421250d
Zeki Mirza <zeki.mirza@googlemail.com>
2021-10-11 21:50:01 +0100

    added id to Todo component

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,6 +1,25 @@
-const todo = (name,dueDate,...tags) => {
-    const isCompleted = false
-    const tagList = [...tags]
+let taskNumber = 0
+
+const Todo = () => {
+    let name
+    let dueDate
+    let isCompleted = false
+    let taskId = taskNumber
+    const tagList = []
+
+    const getCompletionState = () => isCompleted
+
+    const getName = () => name
+
+    const setName = newName => {
+        name = newName
+    }
+
+    const getDate = () => dueDate
+
+    const setDate = newDate => {
+        dueDate = newDate
+    }
 
     const setCompleted = () => {
         isCompleted = true
@@ -10,15 +29,28 @@ const todo = (name,dueDate,...tags) => {
         isCompleted = false
     }
 
-    const addTags = (...newTags) => {
+    const getTags = () => {
+        return tagList
+    }
+
+    const addTags = (newTags) => {
         tagList.push(...newTags)
     }
 
-    const removeTags = (...remvovedTags) => {
+    const removeTags = removedTags => {
         for(let i = 0; i < removedTags.length; i++){
-            tagList.splice(i,1)
+            let positionToRemove = tagList.indexOf(i)
+            tagList.splice(positionToRemove,1)
         }
     }
 
-    return {name,dueDate,isCompleted,tagList,addTags,removeTags,setCompleted,setUncompleted}
+    const increaseTaskNumber = (() => {
+        taskNumber++
+    })()
+
+    const getTaskId = () => taskId
+
+    return {getName,setName,getDate,setDate,getCompletionState,getTags,addTags,removeTags,setCompleted,setUncompleted,getTaskId}
 }
+
+export default Todo
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-const test = document.createElement('p')
-test.textContent = "test"
-document.body.appendChild(test)
+import Todo from './components/todo.js'
+
+let btn = document.createElement('button')
+btn.textContent = "Click to make a task"
+btn.addEventListener('click', () => {
+    let todo = Todo()
+    todo.setName('this is the task')
+    console.log(todo.getTaskNumber())
+})
+
+document.body.appendChild(btn)
